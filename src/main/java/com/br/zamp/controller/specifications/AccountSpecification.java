927f0e0c5f3689118dcecca0797783119b723825
Gabriel Costa <devgabcosta@gmail.com>
2024-02-21 05:30:48 -0300

    fix: specification bugfix

--- src/main/java/com/br/zamp/controller/specifications/AccountSpecification.java
+++ src/main/java/com/br/zamp/controller/specifications/AccountSpecification.java
@@ -1,8 +1,14 @@
 package com.br.zamp.controller.specifications;
 
 import com.br.zamp.domain.Account;
+import net.kaczmarzyk.spring.data.jpa.domain.Like;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Or;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
 
-
+@Or({
+  @Spec(path = "name", spec = Like.class),
+  @Spec(path = "code", spec = Like.class)
+})
 public interface AccountSpecification extends Specification<Account> {
 }
