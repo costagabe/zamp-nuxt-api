efb981f1fa2f7014a84d8b5e1e7bbd5f2f9c1823
Gabriel Costa <devgabcosta@gmail.com>
2024-02-21 05:32:19 -0300

    chore: specification validating level before fetch

--- src/main/java/com/br/zamp/service/impl/UserProfileServiceImpl.java
+++ src/main/java/com/br/zamp/service/impl/UserProfileServiceImpl.java
@@ -4,7 +4,9 @@ import com.br.zamp.config.dto.SelectOption;
 import com.br.zamp.domain.User;
 import com.br.zamp.domain.UserProfile;
 import com.br.zamp.exceptions.ObjectNotFoundException;
+import com.br.zamp.exceptions.ProfileLevelException;
 import com.br.zamp.repository.UserProfileRepository;
+import com.br.zamp.security.AuthenticatedUser;
 import com.br.zamp.service.UserProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -20,15 +22,19 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class UserProfileServiceImpl implements UserProfileService {
   private final UserProfileRepository userProfileRepository;
+  private final AuthenticatedUser authenticatedUser;
 
   @Override
-  public UserProfile create(UserProfile user) {
-    return userProfileRepository.save(user);
+  public UserProfile create(UserProfile profile) {
+    validateUserProfiles(profile);
+    return userProfileRepository.save(profile);
   }
 
   @Override
-  public UserProfile update(UserProfile user) {
-    return null;
+  public UserProfile update(UserProfile profile) {
+    validateUserProfiles(profile);
+
+    return userProfileRepository.save(profile);
   }
 
   @Override
@@ -39,6 +45,19 @@ public class UserProfileServiceImpl implements UserProfileService {
 
   @Override
   public Page<UserProfile> findAll(Specification<UserProfile> specification, Pageable pageable) {
+    var level = authenticatedUser.getUser().getMaxUserProfileLevel().getLevel();
+
+    if (specification == null) {
+      specification = (root, query, criteriaBuilder) ->
+        criteriaBuilder.lessThanOrEqualTo(root.get("level"), criteriaBuilder.literal(level));
+    } else {
+      specification.and((root, query, criteriaBuilder) ->
+        criteriaBuilder.lessThanOrEqualTo(root.get("level"), criteriaBuilder.literal(level)));
+    }
+
+    specification.and((root, query, criteriaBuilder) ->
+      criteriaBuilder.lessThanOrEqualTo(root.get("level"), criteriaBuilder.literal(level)));
+
     return userProfileRepository.findAll(specification, pageable);
   }
 
@@ -64,5 +83,12 @@ public class UserProfileServiceImpl implements UserProfileService {
   @Override
   public Set<UserProfile> findByIdIn(Set<UUID> ids) {
     return userProfileRepository.findByIdIn(ids);
+  }
+
+  private void validateUserProfiles(UserProfile profile) {
+    if (authenticatedUser.getUser().getMaxUserProfileLevel().getLevel() < profile.getLevel()) {
+      throw new ProfileLevelException("Usuário não criar/editar ter um perfil com nível maior que o seu próprio.");
+    }
+
   }
 }
